cbebf58b65aa861d2e1cabc829a4cacfc128ab58
mun9769 <mun9769@users.noreply.github.com>
2020-12-24 00:56:00 +0900

    BlackJack

---
+++ 2789_BlackJack.py
@@ -0,0 +1,20 @@
+import itertools
+import sys
+# N, M = map(int, sys.stdin.readline().split())
+# card = list(map(int,sys.stdin.readline().split()))
+
+N = 10
+M = 500
+card = [93,181,245,214,315,36,185,138,216,295]
+card.sort()
+tmp = []
+for i in range(N-2):
+    for j in range(i+1,N-1):
+        for z in range(j+1,N):
+            if card[i]+card[j]+card[z] > M:
+                continue
+            else :
+                tmp.append(card[i]+card[j]+card[z])
+print(tmp)
+tmp.sort()
+print(tmp[len(tmp)-1])
